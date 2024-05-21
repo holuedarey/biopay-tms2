f984c32f12abf34a37f9090c4897c0f074d37d1e
Samuel Ogunwole <holudare2076@gmail.com>
2024-05-21 14:41:31 +0100

    add virtual Account

--- app/Http/Controllers/Api/Banks.php
+++ app/Http/Controllers/Api/Banks.php
@@ -6,13 +6,16 @@ use App\Contracts\TransferServiceInterface;
 use App\Helpers\MyResponse;
 use App\Http\Controllers\Controller;
 use App\Models\Bank;
+use Opcodes\LogViewer\Logs\Log;
 
 class Banks extends Controller
 {
     public function index(TransferServiceInterface $transferService)
     {
         start:
+        \Illuminate\Support\Facades\Log::notice($transferService::name());
         $banks = Bank::whereProvider($transferService::name())->orderBy('name')->get();
+        \Illuminate\Support\Facades\Log::notice($banks);
 
         if ($banks->isEmpty()) {
             $res = $transferService->updateBankList();
