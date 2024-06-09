bd02b186826ce7287e6715cd4094e16de6b0b31d
Samuel Ogunwole <holudare2076@gmail.com>
2024-06-09 14:21:13 +0100

    fixed revenue

--- app/Helpers/g_functions.php
+++ app/Helpers/g_functions.php
@@ -87,7 +87,7 @@ function providerCharges(float|null $charges, float|null $value, string $service
             $serviceCONFIG = Service::whereSlug($service)->first();
             \Illuminate\Support\Facades\Log::error(json_encode($serviceCONFIG));
             if (!empty($serviceCONFIG)){
-                $configCharge = \App\Models\Fee::where('amount', $serviceCONFIG->id)->first();
+                $configCharge = \App\Models\Fee::where('service_id', $serviceCONFIG->id)->first();
                 return  $configCharge->amount - (($value * $serviceCharge[$service]) / 100) ;
             }
         }
