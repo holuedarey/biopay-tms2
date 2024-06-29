a27774fcd5f30b86247ee4fe495cb9cd5b8f6a55
Victor Akande <akandevictor846@gmail.com>
2024-06-29 16:24:25 +0100

    update role update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,23 +15,23 @@ class DatabaseSeeder extends Seeder
     {
         // create default configs
 
-        $this->call([
-           // ConfigSeeder::class,
-            //RoleSeeder::class,
-            RoutingSeeder::class,
-            //DocumentSeeder::class,
-           // KycLevelSeeder::class,
-//            UserSeeder::class,
-//            ServiceSeeder::class,
-//            TerminalGroupSeeder::class,
-//            FeeSeeder::class,
-//            GeneralLedgerSeeder::class,
-//            GLTSeeder::class,
-//            TerminalSeeder::class,
-            TransactionSeeder::class,
-        ]);
+//        $this->call([
+//           // ConfigSeeder::class,
+//            //RoleSeeder::class,
+//            RoutingSeeder::class,
+//            //DocumentSeeder::class,
+//           // KycLevelSeeder::class,
+////            UserSeeder::class,
+////            ServiceSeeder::class,
+////            TerminalGroupSeeder::class,
+////            FeeSeeder::class,
+////            GeneralLedgerSeeder::class,
+////            GLTSeeder::class,
+////            TerminalSeeder::class,
+//            TransactionSeeder::class,
+//        ]);
 
-//        $dump = \File::get(database_path('seeders/dump.sql'));
-//        \DB::connection()->getPdo()->exec($dump);
+        $dump = \File::get(database_path('seeders/dump.sql'));
+        \DB::connection()->getPdo()->exec($dump);
     }
 }
